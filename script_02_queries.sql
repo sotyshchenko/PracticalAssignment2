34004b9dd517ecbd287afb960caa371839412b69
Sofiia Tyshchenko <styshchenko@kse.org.ua>
2024-06-26 22:22:12 +0300

    Fifth Commit
    Fixed issues

--- script_02_queries.sql
+++ script_02_queries.sql
@@ -22,30 +22,36 @@ drop index idx_opt_client_status
 -- Non-optimized
    
 explain analyze 
-select oc.id as client_id, oo.order_id, oo.order_date, oo.product_id, op.product_name, op.product_price
-from opt_clients as oc
-join opt_orders as oo on oc.id = oo.client_id
-join opt_products as op on oo.product_id = op.product_id
-where oo.order_date > '2024-06-01'
-and oo.order_id like '_____'
-and (op.product_category in ('Category1', 'Category3', 'Category5'))
-and op.product_price > 100
-and oc.status = 'active'
-order by op.product_price desc;   
-
-
-
-explain analyze 
-select count(oc.id), sum(op.product_price) 
-from opt_clients as oc
-join opt_orders as oo on oc.id = oo.client_id
-join opt_products as op on oo.product_id = op.product_id
-where oo.order_date > '2024-06-01'
-and oo.order_id like '_____'
-and (op.product_category in ('Category1', 'Category3', 'Category5'))
-and op.product_price > 100
-and oc.status = 'active';
-  
+select
+    (select cnt
+     from (select count(*) as cnt
+           from (select oc.id as client_id, oo.order_id, oo.order_date, oo.product_id, op.product_name, op.product_price
+                 from opt_clients as oc
+                 join opt_orders as oo on oc.id = oo.client_id
+                 join opt_products as op on oo.product_id = op.product_id
+                 where oo.order_date > '2024-06-01'
+                 and oo.order_id like '_____'
+                 and op.product_category in ('Category1', 'Category3', 'Category5')
+                 and op.product_price > 100
+                 and oc.status = 'active') as sub1
+           ) as sub2
+     limit 1) as count_orders,
+     
+    (select sm 
+     from (select sum(product_price) as sm
+           from (select oc.id as client_id, oo.order_id, oo.order_date, oo.product_id, op.product_name, op.product_price
+                 from opt_clients as oc
+                 join opt_orders as oo on oc.id = oo.client_id
+                 join opt_products as op on oo.product_id = op.product_id
+                 where oo.order_date > '2024-06-01'
+                 and oo.order_id like '_____'
+                 and op.product_category in ('Category1', 'Category3', 'Category5')
+                 and op.product_price > 100
+                 and oc.status = 'active') as sub3
+           ) as sub4
+     limit 1) as orders_value;
+		
+				
 
 
 
@@ -64,27 +70,9 @@ create index idx_opt_client_status
 	on opt_clients(status);
 
 
-
 explain analyze
 with cte as (
-    select oc.id as client_id, oo.order_id, oo.order_date, oo.product_id, op.product_name, op.product_price
-    from opt_clients as oc
-    join opt_orders as oo on oc.id = oo.client_id
-    join opt_products as op on oo.product_id = op.product_id
-    where oo.order_date > '2024-06-01'
-    and oo.order_id like '_____'
-    and op.product_category in ('Category1', 'Category3', 'Category5')
-    and op.product_price > 100
-    and oc.status = 'active'
-    )
-
-select * from cte order by op.product_price desc;   
-
-
-
-explain analyze
-with cte as (
-    select count(oc.id), sum(op.product_price)
+    select count(*) as count_orders, sum(op.product_price) as orders_value
     from opt_clients as oc
     join opt_orders as oo on oc.id = oo.client_id
     join opt_products as op on oo.product_id = op.product_id
